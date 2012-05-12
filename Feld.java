659011aba3f2fed39f4ffb6e3235302a08108d7a
Martin van Arkel <martinva@hotmail.de>
2012-05-12 18:40:59 +0200

    Die Klasse Feld hinzugefuegt

---
+++ Feld.java
@@ -0,0 +1,12 @@
+//Oberklasse von der sämtliche Felder des Spielfeldes
+//abgeleitet werden können
+
+import java.io.Serializable; 
+//Serializable zum Speichern des Objektes
+
+public abstract class Feld implements Serializable {
+	
+	public void draw(){
+	}
+	
+}
