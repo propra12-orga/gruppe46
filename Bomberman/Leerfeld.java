3f3a48bb0fef536846737300afe14e574b566af4
Raphael <Raphael.Podoba@uni-duesseldorf.de>
2012-06-13 23:50:44 +0200

    auf ein neues

--- Bomberman/Leerfeld.java
+++
@@ -1,8 +0,0 @@
-//Ein einfaches leeres Feld
-public class Leerfeld extends Feld{
-
-	public void draw(int x, int y){
-		Renderer.Tile_Empty.draw(x * 128 * 0.33f, y * 128 * 0.33f);
-		
-	}
-}
--- Bomberman/Steinfeld.java
+++
@@ -1,9 +0,0 @@
-
-//Steinfelder sind nicht durch Bomben zerstoerbar
-public class Steinfeld extends Feld{
-	
-	
-	public void draw(int x, int y){
-		Renderer.Tile_Wall.draw(x * 128 * 0.33f, y * 128 * 0.33f);
-	}
-}
