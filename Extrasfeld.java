34d2eb60e8b1b677c6cbee2a874be89bea407515
Philip Höfges <philip@philip-AOD255.(none)>
2012-06-20 08:59:46 +0200

    Vorbereitung fuer weitere Gadgets

---
+++ Extrasfeld.java
@@ -0,0 +1,16 @@
+/**
+ * Klasse fuer Extrasfelder
+ * @author philip
+ *
+ */
+public class Extrasfeld extends Feld
+{
+	public void draw(int x, int y)
+	{
+		/*
+		 * Variable fuer das jeweilige Extra; Aufruf einer getter
+		 * Renderer.Tile_   .draw(x * 128 * 0.33f, y * 128 * 0.33f);
+		 * Switch fuer Unterschiede 
+		 */
+	}
+}
