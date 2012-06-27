a27317d8713398df908f647e3c7c3adab25b0af0
Martin van Arkel <martinva@hotmail.de>
2012-06-27 20:15:58 +0200

    Leveleditor um Startpositionen und beliebige Groesse erweitert. Level1.xml und Level2.xml erstellt.

--- Game.java
+++ Game.java
@@ -26,7 +26,7 @@ public class Game implements Runnable {
 	public Game(int spielerzahl) {
 		//Spielfeld laden
 		try {
-			initialfeld("default.xml");
+			initialfeld("Level2.xml", spielerzahl); 
 		} catch (FileNotFoundException e) {
 			// Level nicht gefunden
 			e.printStackTrace();
@@ -35,83 +35,9 @@ public class Game implements Runnable {
 		}
 		//
 		
-		if(spielerzahl==1) {
-			players.add(new Player("One",1,1));
-		}
-		if(spielerzahl==2) {
-			players.add(new Player("One",1,1));
-			players.add(new Player("Two",13,11));
-		}	
-		//Ausgangsfeld mit zufaelligen Variablen
-		int x = 1;
-		int y = 1;
-		do {
-			x = (int) ((Math.random()*100)%13)+1;
-			y = (int) ((Math.random()*100)%11)+1;
-		}
-		while(spielfeld[x][y] instanceof Steinfeld);
-
-		spielfeld[x][y]=new Exitfeld();
-		int a = 1;
-		int b = 1;
-		do {
-			a = (int) ((Math.random()*100)%13)+1;
-			b = (int) ((Math.random()*100)%11)+1;
-		}
-		while(spielfeld[a][b] instanceof Steinfeld || spielfeld[a][b] instanceof Exitfeld || spielfeld[a][b] instanceof Extrasfeld);
-
-		int art = 1;
-		arr = new boolean[13];
-		for(int i=0;i<arr.length;i++)
-		{
-			arr[i] = true;
-		}
 		
-		if(Main.m.getPowerUps())
-		{
-			for(int i=0;i<5;i++)
-			{
-				int zahl = (int)((Math.random()*100)%100)+1;
-				
-					if(zahl>=1 && zahl<10 && arr[0] == true)
-					{ 	art = 1;
-						arr[0] = false;
-					}
-					//if(arr[0] == false) i--;
-					
-					if(zahl>=10 && zahl<26 && arr[1] == true)
-					{ 	art = 2;
-					arr[1] = false;
-					}
-					//if(arr[1] == false) i--;
-					
-					if(zahl>=26 && zahl<35 && arr[2] == true)
-					{ 	art = 3;
-					arr[2] = false;
-					}
-					//if(arr[2] == false) i--;
-					
-					if(zahl>=35 && zahl<40 && arr[3] == true)
-					{ 	art = 4;
-					arr[3] = false;
-					}
-					//if(arr[3] == false) i--;
-					
-					if(zahl>=40 && zahl<49 && arr[4] == true){ art = 5;}
-					if(zahl>=49 && zahl<53 && arr[5] == true){ art = 6;}
-					if(zahl>=53 && zahl<62 && arr[6] == true){ art = 7;}
-					if(zahl>=62 && zahl<66 && arr[7] == true){ art = 8;}
-					if(zahl>=66 && zahl<70 && arr[8] == true){ art = 9;}
-					if(zahl>=70 && zahl<73 && arr[9] == true){ art = 10;}
-					if(zahl>=73 && zahl<77 && arr[10] == true){ art = 11;}
-					if(zahl>=77 && zahl<86 && arr[11] == true){ art = 12;}
-					if(zahl>=86 && zahl<100 && arr[12] == true){ art = 13;}
-				
-			}
-		}
-		// art = Algorithmus... For-Schleife inklusive boolean um erneut zu generieren um mehrfach Extrafeld zu vermeiden
-		spielfeld[x][y]=new Extrasfeld(art);
-		GameTime.init();
+		
+		
 	}
 	/**
 	 * Veraendert die Zustaende des Arrays bzgl. der Anzahl der Power-Ups
@@ -323,36 +249,49 @@ public class Game implements Runnable {
  * @throws FileNotFoundException
  * @throws XMLStreamException
  */
-	protected void initialfeld(String name) throws FileNotFoundException, XMLStreamException{
+	protected void initialfeld(String name, int spielerzahl) throws FileNotFoundException, XMLStreamException{
 		InputStream in = new FileInputStream(name);
 		XMLInputFactory factory = XMLInputFactory.newInstance();
 		XMLStreamReader parser = factory.createXMLStreamReader(in);	
-		
-		spielfeld=new Feld[15][13];
-		int y = -2;		//2mal Start_Element bevor die Attribute kommen, somit y=0 beim ersten Attribut
+		int breit=0, hoch=0;
+		int y = -1;		//2mal Start_Element bevor die Attribute kommen, somit y=0 beim ersten Attribut
 		
 		while( parser.hasNext() ) {
 		    int event = parser.next();
-		    if (event==XMLStreamConstants.START_ELEMENT) y++;
+		   
 		    switch (event) {
 		        case XMLStreamConstants.END_DOCUMENT:
 		            parser.close();
 		            break;
 		        case XMLStreamConstants.START_ELEMENT:
-		            for( int x = 0; x < parser.getAttributeCount(); x++ ){
-		            	String test = parser.getAttributeValue(x);
+		        	if(parser.getLocalName().equals("Groesse")){
+		        		breit= Integer.parseInt(parser.getAttributeValue(0));
+		        		hoch= Integer.parseInt(parser.getAttributeValue(1));
+		        		spielfeld= new Feld[breit][hoch];
+		        	}
+		        	else if(parser.getLocalName().equals("Startpositionen")){
+		        		int x1 = Integer.parseInt(parser.getAttributeValue(0));
+		        		int y1 = Integer.parseInt(parser.getAttributeValue(1));
+		        		int x2 = Integer.parseInt(parser.getAttributeValue(2));
+		        		int y2 = Integer.parseInt(parser.getAttributeValue(3));
+		        		
+		        		if(spielerzahl==1) {
+		        			players.add(new Player("One",x1,y1));
+		        		}
+		        		if(spielerzahl==2) {
+		        			players.add(new Player("One",x1,y1));
+		        			players.add(new Player("Two",x2,y2));
+		        		}
+		        	} else if(parser.getLocalName().startsWith("Zeile")){
+		        		y++;
+		        		for( int x = 0; x < parser.getAttributeCount(); x++ ){
+		        			String test = parser.getAttributeValue(x);
 		            	
-		            		if (test.equals("Steinfeld")){spielfeld[x][y]= new Steinfeld();}
-		            		else if (test.equals("Mauerfeld")){spielfeld[x][y]= new Mauerfeld();}
-		            		else if (test.equals("Leerfeld")){spielfeld[x][y]= new Leerfeld();};
-		            		
-		            		/*switch(parser.getAttributeValue(x)){
-		            			case "Steinfeld": spielfeld[x][y]= new Steinfeld(); break;
-		            			case "Mauerfeld": spielfeld[x][y]= new Mauerfeld(); break;
-		            			case "Leerfeld": spielfeld[x][y]= new Leerfeld(); break;
-		            			default: break;
-		            		}*/
-		            }
+		            		if (test.equals("S")){spielfeld[x][y]= new Steinfeld();}
+		            		else if (test.equals("M")){spielfeld[x][y]= new Mauerfeld();}
+		            		else if (test.equals("L")){spielfeld[x][y]= new Leerfeld();}
+		        		}//for
+		        	}//if 
 		            break;
 		        
 		       
@@ -362,7 +301,77 @@ public class Game implements Runnable {
 		}
 		
 		
+
+		//Ausgangsfeld mit zufaelligen Variablen
+		int a = 1;
+		int b = 1;
+		do {
+			a = (int) ((Math.random()*100)%(breit-2))+1;
+			b = (int) ((Math.random()*100)%(hoch-2))+1;
+		}
+		while(spielfeld[a][b] instanceof Steinfeld);
+
+		spielfeld[a][b]=new Exitfeld();
+		a = 1;
+		b = 1;
+		do {
+			a = (int) ((Math.random()*100)%(breit-2))+1;
+			b = (int) ((Math.random()*100)%(hoch-2))+1;
+		}
+		while(spielfeld[a][b] instanceof Steinfeld || spielfeld[a][b] instanceof Exitfeld || spielfeld[a][b] instanceof Extrasfeld);
+		/*
+		int art = 1;
+		arr = new boolean[13];
+		for(int i=0;i<arr.length;i++)
+		{
+			arr[i] = true;
+		}
 		
+		if(Main.m.getPowerUps())
+		{
+			for(int i=0;i<5;i++)
+			{
+				int zahl = (int)((Math.random()*100)%100)+1;
+				
+					if(zahl>=1 && zahl<10 && arr[0] == true)
+					{ 	art = 1;
+						arr[0] = false;
+					}
+					//if(arr[0] == false) i--;
+					
+					if(zahl>=10 && zahl<26 && arr[1] == true)
+					{ 	art = 2;
+					arr[1] = false;
+					}
+					//if(arr[1] == false) i--;
+					
+					if(zahl>=26 && zahl<35 && arr[2] == true)
+					{ 	art = 3;
+					arr[2] = false;
+					}
+					//if(arr[2] == false) i--;
+					
+					if(zahl>=35 && zahl<40 && arr[3] == true)
+					{ 	art = 4;
+					arr[3] = false;
+					}
+					//if(arr[3] == false) i--;
+					
+					if(zahl>=40 && zahl<49 && arr[4] == true){ art = 5;}
+					if(zahl>=49 && zahl<53 && arr[5] == true){ art = 6;}
+					if(zahl>=53 && zahl<62 && arr[6] == true){ art = 7;}
+					if(zahl>=62 && zahl<66 && arr[7] == true){ art = 8;}
+					if(zahl>=66 && zahl<70 && arr[8] == true){ art = 9;}
+					if(zahl>=70 && zahl<73 && arr[9] == true){ art = 10;}
+					if(zahl>=73 && zahl<77 && arr[10] == true){ art = 11;}
+					if(zahl>=77 && zahl<86 && arr[11] == true){ art = 12;}
+					if(zahl>=86 && zahl<100 && arr[12] == true){ art = 13;}
+				
+			}
+		}
+		// art = Algorithmus... For-Schleife inklusive boolean um erneut zu generieren um mehrfach Extrafeld zu vermeiden
+		spielfeld[x][y]=new Extrasfeld(art);
+		GameTime.init();*/
 		
 		
 	}
--- XMLlevel.java
+++ XMLlevel.java
@@ -23,13 +23,31 @@ public class XMLlevel {
 		BufferedReader eingabe = new BufferedReader(new InputStreamReader(System.in));		
 		System.out.println("Wie soll das neue Level heissen?");
 		String name="";
+		int breit=0;
+		int hoch=0;
+		int position[] = new int[4];
 		try {
 			name = eingabe.readLine();
+			System.out.println("Wie breit soll das Level " + name +" werden?");
+			breit = Integer.parseInt(eingabe.readLine());
+			System.out.println("Wie hoch soll das Level " + name + " werden?");
+			hoch = Integer.parseInt(eingabe.readLine());
+			System.out.println("An welchen Positionen sollen die Spieler starten?");
+			for (int i=0; i<4; i++){
+			System.out.print("x: ");
+			position[i] = Integer.parseInt(eingabe.readLine());
+			System.out.println();
+			i++;
+			System.out.print("y: ");
+			position[i] = Integer.parseInt(eingabe.readLine());
+			}//for
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+			System.exit(1);
 		}
-		Feld[][] spielfeld = new Feld[15][13];
+
+		Feld[][] spielfeld = new Feld[breit][hoch];	
 		
 		
 		//erstellen der xml-Datei
@@ -38,11 +56,19 @@ public class XMLlevel {
 		
 		
 		//schreiben der xml-Datei
+		//zunaechst die Groesse des Spielfeldes
 		schreiber.writeStartDocument();
+		schreiber.writeStartElement("Level");
+		schreiber.writeStartElement("Groesse");
+		schreiber.writeAttribute("Breite", String.valueOf(breit));
+		schreiber.writeAttribute("Hoehe", String.valueOf(hoch));
+		schreiber.writeEndElement();
+		
+		//hier die Beschaffenheit der einzelnen Felder
 		schreiber.writeStartElement("Spielfeld");
-		for (int i=0;i<13;i++){
+		for (int i=0;i<hoch;i++){
 			schreiber.writeStartElement("Zeile"+String.valueOf(i));
-			for (int j=0;j<15;j++){
+			for (int j=0;j<breit;j++){
 				System.out.println("Position X:"+ (j+1) +" Y:"+ (i+1) + ". Welche Art von Feld soll an dieser Position sein? ('S'= nicht zerstoerbarer Stein, 'L'= leeres Feld, 'M'= Mauer)");
 				BufferedReader eing = new BufferedReader(new InputStreamReader(System.in));
 				String zeile="";
@@ -55,11 +81,11 @@ public class XMLlevel {
 				char auswahl = zeile.charAt(0);
 				//System.out.println(auswahl);
 				switch(auswahl){
-					case 'S': schreiber.writeAttribute("X"+String.valueOf(j),"Steinfeld");
+					case 'S': schreiber.writeAttribute("X"+String.valueOf(j),"S");
 								break;
-					case 'L': schreiber.writeAttribute("X"+String.valueOf(j),"Leerfeld");
+					case 'L': schreiber.writeAttribute("X"+String.valueOf(j),"L");
 								break;
-					case 'M': schreiber.writeAttribute("X"+String.valueOf(j), "Mauerfeld");
+					case 'M': schreiber.writeAttribute("X"+String.valueOf(j), "M");
 								break;
 					default: System.out.println("Fehlerhafte Eingabe. Bitte Programm erneut starten.");
 								System.exit(1);
@@ -68,6 +94,14 @@ public class XMLlevel {
 			}
 			schreiber.writeEndElement();
 		}//end for(i)
+		schreiber.writeEndElement();
+		
+		//festlegen der Startpositionen
+		schreiber.writeStartElement("Startpositionen");
+		for (int i=0; i<4; i++){
+			schreiber.writeAttribute("pos"+i, String.valueOf(position[i]));
+		}
+		schreiber.writeEndElement();
 		schreiber.writeEndElement();
 		schreiber.writeEndDocument();
 		schreiber.close();
